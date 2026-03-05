0043aec6e3419a6ae9bc5c1fede213e9a50edd59
Jessica-Curtinaz <jessicacurtinaz@gmail.com>
2026-03-05 14:47:47 -0300

    feat: adiciona tela de login

---
+++ src/app/login/login.page.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { IonicModule } from '@ionic/angular';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+  standalone: true,
+  imports: [IonicModule, CommonModule, FormsModule]
+})
+export class LoginPage implements OnInit {
+  usuario = '';
+  senha = '';
+
+  constructor(private router: Router) { }
+
+  ngOnInit() { }
+
+  fazerLogin() {
+    if (this.usuario === 'admin' && this.senha === 'admin') {
+      alert('Login efetuado com sucesso!');
+      console.log('Login efetuado com sucesso no Sistema Santa Hedviges!');
+    } else {
+      alert('Usuário ou senha incorretos. Tente novamente!');
+    }
+  }
+}
